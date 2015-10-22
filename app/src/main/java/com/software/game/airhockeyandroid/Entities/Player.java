9a8f9092744a8e7ab08e4190d1571b278ba00083
vshekhar89 <vshekhar@umail.iu.edu>
2015-10-21 23:15:50 -0400

    Modified Player class

--- app/src/main/java/com/software/game/airhockeyandroid/Entities/Player.java
+++ app/src/main/java/com/software/game/airhockeyandroid/Entities/Player.java
@@ -31,7 +31,7 @@ public class Player {
         return player;
     }
     public static Player getInstance(){
-         player= new Player();
+         //player= new Player();
         return player;
 
         }
