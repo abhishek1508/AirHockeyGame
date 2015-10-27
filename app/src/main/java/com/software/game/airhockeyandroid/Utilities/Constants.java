55d2464f5810be359aa2235833ac9d9314cdd10e
abhishek1508 <abhi.kejriwal1508@gmail.com>
2015-10-26 22:52:11 -0400

    Added the constants file

---
+++ app/src/main/java/com/software/game/airhockeyandroid/Utilities/Constants.java
@@ -0,0 +1,12 @@
+package com.software.game.airhockeyandroid.Utilities;
+
+/**
+ * Created by Abhishek on 10/26/2015.
+ */
+public class Constants {
+
+    public static final String LOGIN_URL = "http://androidgamedeveloper.site11.com/connection/verifyAndGetDetails.php";
+    public static final String CREATE_PROFILE_URL = "http://androidgamedeveloper.site11.com/connection/create_profile.php";
+    public static final String DELETE_PROFILE_URL = "http://androidgamedeveloper.site11.com/connection/deleteProfile.php";
+    public static final String UPDATE_PROFILE_URL = "http://androidgamedeveloper.site11.com/connection/updateProfile.php";
+}
